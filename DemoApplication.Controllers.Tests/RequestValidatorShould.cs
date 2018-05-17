c770365f39bdb0604d0f09e9cc8521e37f2596f9
Sreekanth Nair <sreekanth.nair@asos.com>
2018-05-17 08:51:50 +0100

    Improving unit test coverage for request validator

--- DemoApplication.Controllers.Tests/RequestValidatorShould.cs
+++ DemoApplication.Controllers.Tests/RequestValidatorShould.cs
@@ -33,6 +33,20 @@ namespace DemoApplication.Controllers.Tests
         }
 
         [Test]
+        public async Task GivenValidInput_ReturnsNoError()
+        {
+            var countries = new List<Country>() { new Country("TS", "Test") };
+
+            _countryService.Setup(x => x.GetCountryByCountryCode("TS"))
+                .Returns(Task.FromResult(countries));
+
+            var validationError = await _requestValidator.Validate(100, "TS", "Swimming");
+
+            validationError.Count.ShouldBe(0);
+        }
+
+
+        [Test]
         public async Task GivenInvalidCountry_ReturnsError()
         {
             _countryService.Setup(x => x.GetCountryByCountryCode("TS"))
@@ -40,8 +54,46 @@ namespace DemoApplication.Controllers.Tests
 
             var validationError = await _requestValidator.Validate(100, "TS", "Swimming");
             
-            //validationError.ErrorMessage.ShouldBe(testErrorMessage);
             validationError.Single().ParameterName.ShouldBe(GiftAidConstants.InputFields.Country);
         }
+
+        [Test]
+        public async Task GivenEmptyCountry_ReturnsError()
+        {
+            var validationError = await _requestValidator.Validate(100, "", "Swimming");
+
+            validationError.Single().ParameterName.ShouldBe(GiftAidConstants.InputFields.Country);
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public async Task GivenInvalidDonation_ReturnsError(int donationAmount)
+        {
+            var countries = new List<Country>() {new Country("TS","Test")};
+
+            _countryService.Setup(x => x.GetCountryByCountryCode("TS"))
+                .Returns(Task.FromResult(countries));
+
+            var validationError = await _requestValidator.Validate(donationAmount, "TS", "Swimming");
+
+            validationError.Single().ParameterName.ShouldBe(GiftAidConstants.InputFields.Donation);
+        }
+
+        [Test]
+        public async Task GivenInvalidType_ReturnsError()
+        {
+            var countries = new List<Country>() { new Country("TS", "Test") };
+
+            _countryService.Setup(x => x.GetCountryByCountryCode("TS"))
+                .Returns(Task.FromResult(countries));
+
+            var validationError = await _requestValidator.Validate(100, "TS", "Invalid");
+
+            validationError.Single().ParameterName.ShouldBe(GiftAidConstants.InputFields.EventType);
+        }
+
+
+
+
     }
 }
